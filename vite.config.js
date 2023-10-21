fbab34cbe9020508a5f22d4e56af416c756c5c91
richardbbonsa <richardbbonsa@gmail.com>
2023-10-21 00:11:45 -0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react-router-dom'],
+      external: ['react-router-dom', 'bootstrap/dist/css/bootstrap.min.css'],
     },
   },
 });
