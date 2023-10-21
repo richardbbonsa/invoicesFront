9ba831c192231ec255a2e142513ff2df9c223484
richardbbonsa <richardbbonsa@gmail.com>
2023-10-21 00:05:56 -0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +7,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@mui/icons-material': '@mui/icons-material/esm',
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
     },
   },
 });
